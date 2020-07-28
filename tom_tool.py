56616fec9c878433609ce3478c96e134a37c30fd
Joris Witteman <joriswitteman@hotmail.com>
2020-07-28 22:45:10 +0200

    Changed to full erase

--- tom_tool.py
+++ tom_tool.py
@@ -23,12 +23,12 @@ print(""" ______________________________________
 
 result = input("Reflash? [Y/n]")
 for item in serial.tools.list_ports.comports():
-    if item.manufacturer == "HackZone" and (result == "" or result == "Y" or result == "Yes" or result == "yes"):    
+    if item.vid == 0xCAFE and (result == "" or result == "Y" or result == "Yes" or result == "yes"):    
         r = requests.get(url, allow_redirects=True)
         if r.ok:
             open('firmware.bin', 'wb').write(r.content)
-            esptool.main(["-b", "115200", "-p", item.device, "erase_flash"])
-            esptool.main(["--baud", "1000000", "--port", item.device, "--before", "default_reset", "--after", "hard_reset", "write_flash", "-z", "--flash_mode", "dio", "--flash_freq", "80m", "--flash_size", "detect", "0x1e1000", "initial_fs.zip", "0xd000", "ota_data_initial.bin", "0x1000", "bootloader.bin", "0x10000", "firmware.bin", "0x8000", "campzone2020_16MB.bin"])
+            esptool.main(["--baud", "115200", "--port", item.device, "erase_flash"])
+            esptool.main(["--baud", "115200", "--port", item.device, "--before", "default_reset", "--after", "hard_reset", "write_flash", "-z", "--flash_mode", "dio", "--flash_freq", "80m", "--flash_size", "detect", "0x1e1000", "initial_fs.zip", "0xd000", "ota_data_initial.bin", "0x1000", "bootloader.bin", "0x10000", "firmware.bin", "0x8000", "campzone2020_16MB.bin"])
             print("""
 Flash succes!
 Further instructions:
